d4c9e4840f58c524404eb95c969ca74829f4720c
Haleem Hosein <haleemhosein@yahoo.com>
2022-11-19 21:22:15 -0400

    03 Creating a base layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
