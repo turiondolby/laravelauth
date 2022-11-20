2d9a586e384f59be095294ae5f781f7ea7a62eaf
Haleem Hosein <haleemhosein@yahoo.com>
2022-11-20 14:36:03 -0400

    05 Authentication primer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,18 +20,23 @@
         </ul>
 
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Haleem Hosein</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
+            @auth
+                <li>
+                    <a href="#" class="p-3">{{ auth()->user()->name }}</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            @endauth
+
+            @guest
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
 
